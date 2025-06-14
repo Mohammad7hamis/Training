5ce7d54536bdcc1c10212e8b3c6a74be2879d932
Mohammad Khamis <bin.khamis93@gmail.com>
2025-06-15 00:07:43 +0300

    first commit

---
+++ 2-Verify password.py
@@ -0,0 +1,10 @@
+while True:
+
+    passwords = str(input("Please Enter The password "))
+
+    if passwords == "pass123":
+        print("Welcom")
+        break
+    
+    else:
+        print("Try Again")
