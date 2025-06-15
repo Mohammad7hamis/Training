317ad2c7e00c59889d38d40d065285986905d5e0
Mohammad Khamis <bin.khamis93@gmail.com>
2025-06-15 22:03:23 +0300

    Added multiplication table and modified two scripts

---
+++ 3-Determine the largest number.py
@@ -0,0 +1,7 @@
+first_number = int(input("Please Enter the First Number: "))
+second_number = int(input("Please Enter the Second Number: "))
+
+if first_number > second_number:
+    print("The greater is", first_number)
+else:
+    print("The greater is", second_number)
---
+++ 5-multiplication table.py
@@ -0,0 +1,4 @@
+number = int(input("Please Enter the number "))
+x = "x"
+for n in range(1, 11):
+    print(number, x, n, "=", number*n)
