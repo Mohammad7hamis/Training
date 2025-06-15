ef43d3f43fef3b7f6b1019b5ec6b609a1897fa81
Mohammad Khamis <bin.khamis93@gmail.com>
2025-06-15 22:23:22 +0300

    The last

---
+++ 6-The program asks the user for a number, then checks.py
@@ -0,0 +1,9 @@
+number = int(input("Enter the Number please! "))
+if number > 100:
+    print("Too big")
+    
+elif number >= 50 and number <= 100:
+    print("In range")
+    
+else:
+    print("Too small")
