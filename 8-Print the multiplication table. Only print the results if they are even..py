258d7b62fc238468a0fc08bfa4ade8534ef89f46
Mohammad Khamis <bin.khamis93@gmail.com>
2025-06-17 00:24:19 +0300

    Today Tue, 17 JUN

--- 8-Print the multiplication table. Only print the results if they are even..py
+++ 8-Print the multiplication table. Only print the results if they are even..py
@@ -4,3 +4,10 @@ x = "x"
 for n in range(1, 5):
     if n*number % 2 == 0 :
         print(number, x, n, "=", number*n)
+
+
+#/ This code will asks the user to enter a number, 
+# then uses a for loop to print the multiplication table
+# for that number from 1 to 5.
+# But only print the results if they are even.
+# Don't use (continue) now, only if inside for.
