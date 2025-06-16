51890063bc5eae28b1e172b20efd20806f976167
Mohammad Khamis <bin.khamis93@gmail.com>
2025-06-16 22:51:03 +0300

    Today Mon, 16 JUN

--- 7-Enter password. It gives only 3 attempts.py
+++ 7-Enter password. It gives only 3 attempts.py
@@ -1,21 +1,4 @@
-<<<<<<< HEAD
 cycle = 0
-
-while cycle < 3:
-
-    passwords = input("Please Enter The password ")
-
-    if passwords == "pass123":
-        print("Access granted")
-        break
-    else:
-        print("Wrong password")
-        cycle += 1
-print("--------------")
-if cycle == 3:
-=======
-cycle = 0
-
 while cycle < 3:
 
     passwords = input("Please Enter The password ")
@@ -28,5 +11,4 @@ while cycle < 3:
         cycle += 1
 print("------------------")
 if cycle == 3:
->>>>>>> d1fcfceaac145ec1d73e0def7f86a83ed7e6263e
     print("Access denied")
