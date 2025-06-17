2c862172a436dba639652a79a46f303b306cd3ca
MOhammad <bin.khamis93@gmail.com>
2025-06-18 00:54:22 +0300

    Today Wed, 18 JUN

--- 9-A program that asks the user to enter 5 numbers (one by one).py
+++ 9-A program that asks the user to enter 5 numbers (one by one).py
@@ -1,10 +1,4 @@
 
 for n in range(5):
     number = int(input("Enter the number of inputs: "))
-    print(number)
-
-    if number >= 5:
-        print(number)
-
-    else:
-        print(number) 
+    
