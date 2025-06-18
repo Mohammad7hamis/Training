ecf49635d1cef52c1cb8e75ac202faf3a0ead8f4
MOhammad <bin.khamis93@gmail.com>
2025-06-18 23:16:42 +0300

    Today Wed, 19 JUN

--- 9-A program that asks the user to enter 5 numbers (one by one).py
+++ 9-A program that asks the user to enter 5 numbers (one by one).py
@@ -1,4 +1,14 @@
+largest = None
+smallest = None
 
-for n in range(5):
-    number = int(input("Enter the number of inputs: "))
-    
+for i in range(5):
+    number = int(input("Enter a number: "))
+
+    if largest is None or number > largest:
+        largest = number
+
+    if smallest is None or number < smallest:
+        smallest = number
+
+print("Largest number is:", largest)
+print("Smallest number is:", smallest)
