ed365cd6a053947ae71b2ad8f41f40a89a1dad10
Mohammad Khamis <bin.khamis93@gmail.com>
2025-06-26 18:43:54 +0300

    Today Tue, 26 JUN

---
+++ 15-Function that displays a student's grade.py
@@ -0,0 +1,18 @@
+'''
+✏️ Write a function called show_grade.
+It takes:
+grades: A dictionary containing student names and grades.
+name: The name of the student whose grade you want to find.
+If the name exists:
+Print: "Grade for <name> is <grade>"
+If it doesn't exist:
+Print: "Student not found."
+'''
+def show_grade(grades, the_names):
+    if the_names in grades:
+        print("Grade for ", the_names, "is",grades[the_names] )
+    else:
+        print("Student not found.")
+grades = {"Mohammad": 95, "Sara": 88, "Ali": 46}
+show_grade(grades, "Sara")
+show_grade(grades, "Huda")
