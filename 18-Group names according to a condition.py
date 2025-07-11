ef3f0cfe6a0b599205b999fbee916280d6d1c352
Mohammad Khamis <bin.khamis93@gmail.com>
2025-07-11 21:53:15 +0300

    Today Fri, 11 JUL

---
+++ 18-Group names according to a condition.py
@@ -0,0 +1,13 @@
+'''
+✏️ Write a function called (collect_short_names):
+that does the following:
+It asks the user to enter a number of names (for example, 3 names).
+It then asks the user to enter each name.
+It stores only names with fewer than 5 characters in a list.
+It returns this list (using return).
+'''
+def collect_short_names():
+   number_of_student = int(input("How many names? "))
+   short_names = []
+   for n in range (number_of_student):
+       name = input(f"Enter the number of {n+1}: ")
