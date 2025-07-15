14b41ba01d6dbd0373993c932f0ca7da2673a313
Mohammad Khamis <141270465+Mohammad7hamis@users.noreply.github.com>
2025-07-15 14:39:13 +0300

    Today Tue, 15 Jul

---
+++ 22-Only students' grades and successful students.py
@@ -0,0 +1,18 @@
+'''
+Write a function called filter_passed_students.
+Use a dictionary containing student names and their grades.
+Extract only students with a grade of 50 or higher.
+Return a new dictionary containing only those who passed.
+Print the number of students who passed, their names, and their grades.
+'''
+def filter_passed_students():
+    passd_student = {}
+    student_name = {"Mohammad": 95,
+                    "Ali": 42,
+                    "Sara": 78,
+                    "Huda": 30 
+                    }
+    for name,grade in student_name.items:
+        if student_name >= 50:
+            print(student_name)
+filter_passed_students()
