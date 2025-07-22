937a8c7b3cb4ef0d3bdf86c27c38fc80a750defa
Mohammad Khamis <bin.khamis93@gmail.com>
2025-07-22 23:59:39 +0300

    Today Tue, 22 JUL

--- 29-Student Info Filter.py
+++ 29-Student Info Filter.py
@@ -9,17 +9,17 @@ We verify that:
 * The age must be a number between 18 and 25 only.
 * The city must be one of: "Riyadh" or "Jeddah" only.
 '''
-def student_info_filter():
+def students_info_filter():
     number_of_student = int(input("How many student do want to add ? "))
     valid_students = {}
-    for name, info in number_of_student:
-        names = input(f"Please Enter the name {n+1} : ")
-        age = int(input(f"Please Enter the age {n+1} : "))
+    for name, info in number_of_student.itims():
+        names = input(f"Please Enter the name {name+1} : ")
+        age = int(input(f"Please Enter the age {name+1} : "))
         city = input("Riyadh or Jeddah: ")
         if names.capitalize() and names.isalpha():
             valid_students[names] = age,city
                 
         
     return valid_students
-result = student_info_filter()
+result = students_info_filter()
 print(result)
