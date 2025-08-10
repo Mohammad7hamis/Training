1cb1bfc6aacd7cf531751d7232f54aaaf8214b41
Mohammad Khamis <bin.khamis93@gmail.com>
2025-08-10 18:19:14 +0300

    Today, Sun 10 AUG

---
+++ 38-Managing an online store.py
@@ -0,0 +1,12 @@
+def create_order(name, *products, **details):
+    
+  print(f"Customer: {name}")
+  print(f"Prosucts: {" ,".join(products)}")
+  
+  print(f"Details: ")
+  
+  for key, value in details.items():
+      print(f"{key.capitalize()}: {value}")
+      
+      
+create_order("Mohammad", "Laptop", "Mouse", address="Riyadh", payment="Credit Card")
